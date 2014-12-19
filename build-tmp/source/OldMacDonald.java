7ace884cd93912f46a6e3ecb6222d0b54356b92f
MostExcellent <dwglover3@gmail.com>
2014-12-19 00:33:30 -0800

    Now returns animalSounds() to a new Farm

--- build-tmp/source/OldMacDonald.java
+++ build-tmp/source/OldMacDonald.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OldMacDonald extends PApplet {
+
 //your variable declarations here
 Farm oldMcDonaldsFarm;
 public void setup() {
@@ -85,5 +101,14 @@ class Farm {
       System.out.println(aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound());
     }
     System.out.println("The cow is known as " + ((NamedCow) aBunchOfAnimals[0]).getName());
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OldMacDonald" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
